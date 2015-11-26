a9f86b429487919b7e4f939aeb82c52ad29b47ac
R.A. Lucas <richard@richardalucas.com>
2015-11-25 17:59:34 -0700

    added git push check

---
+++ git_push_check.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+shopt -s dotglob
+
+dir=$1
+regex="\.git"
+
+for projects in $dir*; do
+  projdir=`pwd`/$projects
+  subdirs="$(ls -al $projdir | grep -o \.git)"
+  if [[ $subdirs =~ $regex ]]
+    then
+      status="$(cd $projdir; git status | grep 'nothing to commit')"
+      if [[ $status != "" ]]
+        then
+          echo $projects is clean
+        else
+          git_status="$(cd $projdir; git status)"
+          branch="$(cd $projdir; git branch | grep -o [[:alnum:]] | tr -d \n[:space:])"
+          #git push origin $branch
+          #echo Pushed: $project to $branch
+          echo Project $projects on $branch is ready to be pushed
+          echo $git_status
+          touch $HOME/git_to_push.txt
+          echo $projects $git_status >> $HOME/git_to_push.txt
+      fi
+  fi
+done
