ba58ef9e45f9754c78547c75e8efd29b6f807563
R.A. Lucas <richard@richardalucas.com>
2015-12-31 14:52:04 -0700

    upating sed_dir

--- sed_dir.sh
+++ sed_dir.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-dir=`pwd`/$1
-find=$2
-replace=$3
+if [ $1 == '--help' ]
+then
+  echo "      -----  How to use sed_dir: ------
 
-grep -rl $find $dir | xargs sed -i '' 's/\'$find'/\'$replace'/g'
+    sed_dir <directory> <string-to-find> <string-to-replace>
 
+    Example:
+      sed_dir my_dir/ myName newName"
+elif [ $# -lt 3 ]
+then
+   echo "Not enough arguments supplied"
+else
+  dir=`pwd`/$1
+  find=$2
+  replace=$3
+
+  grep -rl $find $dir | xargs sed -i '' 's/\'$find'/\'$replace'/g'
+fi
